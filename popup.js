7883ccd73732f0f391deece5212b6d45d72f1765
danyGhassan <dany.ghassa@gmail.com>
2024-05-20 17:31:59 +0200

    test

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
-require('dotenv').config();
-
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
+const client_id = 'dfc28855a0614ada8457d5d64e9873ed';
+const client_secret = 'ee853425fdd14c13907b5a18ca600d04';
 
 document.getElementById('searchButton').addEventListener('click', function() {
   const artistName = document.getElementById('artistName').value;
