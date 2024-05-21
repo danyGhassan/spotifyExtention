a51137aa87756e11c1c0094b956a01dfcb1e5310
danyGhassan <dany.ghassa@gmail.com>
2024-05-21 10:19:30 +0200

    test

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ const client_id = 'dfc28855a0614ada8457d5d64e9873ed';
 const client_secret = 'ee853425fdd14c13907b5a18ca600d04';
 
 document.getElementById('button-92').addEventListener('click', function() {
-  const artistName = document.getElementById('artistName').value;
+  const artistName = document.getElementById('input artistName').value;
   if (artistName) {
     getAccessToken().then(token => {
       searchArtist(token, artistName).then(artist => {
