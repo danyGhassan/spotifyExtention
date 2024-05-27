bb64ec205a9b6a8adb500cdd002cdf62681342a9
danyGhassan <dany.ghassa@gmail.com>
2024-05-27 12:21:43 +0200

    test

--- id.js
+++ id.js
@@ -2,3 +2,4 @@ const client_id = 'dfc28855a0614ada8457d5d64e9873ed';
 const client_secret = 'ee853425fdd14c13907b5a18ca600d04';
 
 module.exports = { client_id };
+module.exports = { client_secret };
