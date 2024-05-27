a97cc88e63de73524427d5e0249e71e7693697c0
danyGhassan <dany.ghassa@gmail.com>
2024-05-27 12:07:52 +0200

    test

--- id.js
+++ id.js
@@ -1,2 +1,2 @@
-const client_id = 'dfc28855a0614ada8457d5d64e9873ed';
-const client_secret = 'ee853425fdd14c13907b5a18ca600d04';
+export const client_id = 'dfc28855a0614ada8457d5d64e9873ed';
+export const client_secret = 'ee853425fdd14c13907b5a18ca600d04';
